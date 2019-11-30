21cac5c2fb2027c3a7460352fcd1e617fdb8241c
lenotian <517614486@qq.com>
2019-11-30 23:11:14 +0800

    管理端页面框架搭建

---
+++ Source/View/admin/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <el-container>
+      <el-header><Header></Header></el-header>
+      <el-container>
+        <el-aside width="200px"><Menu></Menu></el-aside>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script>
+import Menu from '@/components/Menu.vue'
+import Header from "@/components/Header";
+export default {
+  name: 'app',
+  components:{
+    Menu,
+    Header
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+body{
+  padding: 0;
+  margin: 0;
+}
+</style>
