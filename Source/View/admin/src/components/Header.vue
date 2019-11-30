7dcbe2f6f4eddf40e5758c8527cd972c532c3f52
lenotian <517614486@qq.com>
2019-11-30 23:10:03 +0800

    管理端页面框架搭建

---
+++ Source/View/admin/src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="header-container">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Header"
+    }
+</script>
+
+<style scoped>
+    .header-container{
+        background-color: #2c3e50;
+        width: 100vw;
+    }
+</style>
---
+++ Source/View/admin/src/components/Menu.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="menu-container">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Menu",
+        data(){
+            return{
+                menuList:[{
+                    menuName:"仪表盘",
+                    path:"",
+                    enable:true
+                }]
+            }
+        },
+        methods:{
+
+        }
+    }
+</script>
+
+<style scoped>
+    .menu-container{
+        height: 100vh;
+        background-color: #2c3e50;
+    }
+</style>
